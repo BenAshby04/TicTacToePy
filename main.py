eb2a7b671f558bae009af0c67d91d97a1bcaecbd
Ben Ashby <benashby@Bens-MacBook-Air.local>
2023-01-16 21:22:17 +0000

    added grid

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from sys import exit
 
 pygame.init()
 clock = pygame.time.Clock()
-
+pygame.display.set_caption("TicTacToe")
 screen = pygame.display.set_mode((500,500))
 gameloop = True
+
+WHITE = (255,255,255)
 
 while gameloop:
     for event in pygame.event.get(): 
@@ -14,7 +16,11 @@ while gameloop:
             pygame.quit()
             exit() 
     
+    #Lines for grid
     
-    
+    pygame.draw.line(screen, WHITE, (167,85), (167,400),4) #Left verticle
+    pygame.draw.line(screen,WHITE, (333, 85), (333,400),4) #Right verticle
+    pygame.draw.line(screen,WHITE,(50,180),(450,180),4) #Top Horizontal
+    pygame.draw.line(screen,WHITE,(50,300), (450,300),4)
     pygame.display.flip()
     clock.tick(60)
